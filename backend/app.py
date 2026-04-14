77fa5d61d7b4254656aacce1ea1cc183be3e5276
Devanandanan C <nandannand2003@gmail.com>
2026-04-14 20:42:17 +0530

    updated indentation

--- backend/app.py
+++ backend/app.py
@@ -31,4 +31,5 @@ def delete_task(title):
     collection.delete_one({"title":title})
     return jsonify({"message":"Task deleted successfully"})
 
-if __name__=="__main__":    app.run(host="0.0.0.0", port=5000)
+if __name__=="__main__":    
+    app.run(host="0.0.0.0", port=5000)
